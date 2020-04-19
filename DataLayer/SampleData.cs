deab8287a5673e71b3bbd185fd10f47511f116b0
MatGod <mat_gord@mail.ru>
2020-04-19 09:21:01 +0300

    DataLayer

---
+++ DataLayer/SampleData.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using DataLayer.Entityes;
+
+namespace DataLayer {
+	public class SampleData {
+		public static void InitData(EFDBContext context) {
+			if (!context.Directory.Any()) {
+				context.Directory.Add(new Entityes.Directory() {
+					Title = "FirstDirectory", 
+					Html = "<b>DirectoryContent</b>"
+				});
+				context.Directory.Add(new Entityes.Directory() {
+					Title = "SecondDirectory", 
+					Html = "<b>DirectoryContent</b>"
+				});
+				context.SaveChanges();
+			}
+
+			if (!context.Material.Any()) {
+				context.Material.Add(new Entityes.Material() { 
+					Title = "First Material", 
+					Html = "<i>MaterialContent</i>", 
+					DirectoryId = context.Directory.First().Id
+				});
+				context.Material.Add(new Entityes.Material() {
+					Title = "Second Material",
+					Html = "<i>MaterialContent</i>",
+					DirectoryId = context.Directory.Last().Id
+				});
+				context.SaveChanges();
+			}
+		}
+	}
+}
--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -2,18 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace WebApplication
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+namespace WebApplication {
+    public class Program {
+        public static void Main(string[] args) {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope()) {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<EFDBContext>();
+                SampleData.InitData(context);
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
